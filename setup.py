fedfdbfb9f125cad0e2b32d50ece744cf48272ee
Kavinmozhi <skavinmozhi@gmail.com>
2025-10-04 15:54:34 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'AI Health Assistant Project',
+    version= '0.0.0',
+    author= 'Kavinmozhi Sivakumar',
+    author_email= 'skavinmozhi@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
